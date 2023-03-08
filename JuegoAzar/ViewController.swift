2d9738929dd56328da90b47dab10d4b4121d7fb1
Ricardo <ricardomon85@gmail.com>
2023-03-08 11:52:24 -0600

    mostrar numero aleatorio

--- JuegoAzar/ViewController.swift
+++ JuegoAzar/ViewController.swift
@@ -8,21 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var currentValue: Int = 50
+    @IBOutlet var slider: UISlider!
+    //Etiqueta para mostrar el valor de targetValue
+    @IBOutlet var targetLabel: UILabel!
+    
+    //guarda el valor del slider
+    var currentValue: Int = 0
+    //valor objetivo es el valor que el usuario debe atinarle
+    var targetValue = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //mandamos llamar el metodo para iniciar la ronda numero 1
+        startNewRound()
+    }
+    
+    //metodo que usamos para crear una nueva ronda
+    //seteamos todos lo valores por defecto y mandamos llamar aun nuevo numero aletorio
+    func startNewRound(){
+        self.targetValue = Int.random(in: 1...100)
+        self.currentValue = 50
+        self.slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    //metodo para asiganar el valor correspondiente a todas las label que tenemos en storyboard
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
     }
     
     @IBAction func showAlert(){
-        let message = "El valor del slider is now \(currentValue)"
+        let message = "El valor del slider is now \(currentValue)" + "\nel valor objetivo es \(targetValue)"
         let alert = UIAlertController(title: "Hello, Word", message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        //depues que presentamos todo la infomacion, mandamos llamar el metodo para iniciar una nueva ronda
+        startNewRound()
     }
 
     
